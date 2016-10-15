86c69fcbd052aa0d22e80de9e10dd1d2670c98a7
Vladimír Macháček <8machy@seznam.cz>
2016-10-15 23:20:11 +0200

    sass-migration: added autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp  = require('gulp'),
 	sass = require('gulp-sass'),
 	minifyCss = require('gulp-minify-css'),
+	autoprefixer  = require('gulp-autoprefixer'),
 	rename = require('gulp-rename');
 
 // Setup
@@ -15,12 +16,18 @@ var distDir 		= "./dist",
 gulp.task('sass', function() {
 	gulp.src(targetFile)
 		.pipe(sass())
+		.pipe(autoprefixer({
+			browsers: ['last 2 version']
+		}))
 		.pipe(gulp.dest(distDir))
 });
 
 gulp.task('sass-min', function() {
 	gulp.src(targetFile)
 		.pipe(sass())
+		.pipe(autoprefixer({
+			browsers: ['last 2 version']
+		}))
 		.pipe(minifyCss())
 		.pipe(rename({
 			suffix: '.min'
