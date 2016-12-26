14316dca4b150f6f519783c21f2c618f74eed0e5
Vladimír Macháček <8machy@seznam.cz>
2016-12-26 12:23:17 +0100

    - travis implementation

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/Gridtest_Border_radius_0_document_0_Default.png",
-        "test": "../bitmaps_test/20161226-115400/Gridtest_Border_radius_0_document_0_Default.png",
+        "test": "../bitmaps_test/20161226-122216/Gridtest_Border_radius_0_document_0_Default.png",
         "selector": "document",
         "fileName": "Gridtest_Border_radius_0_document_0_Default.png",
         "label": "Border radius",
@@ -15,8 +15,8 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.01",
-          "analysisTime": 109,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 107,
           "getDiffImage": null
         }
       },
@@ -25,94 +25,10 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/Gridtest_Columns_0_document_0_Default.png",
-        "test": "../bitmaps_test/20161226-115400/Gridtest_Columns_0_document_0_Default.png",
+        "test": "../bitmaps_test/20161226-122216/Gridtest_Columns_0_document_0_Default.png",
         "selector": "document",
         "fileName": "Gridtest_Columns_0_document_0_Default.png",
         "label": "Columns",
-        "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00",
-          "analysisTime": 94,
-          "getDiffImage": null
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/Gridtest_Display_types_0_document_0_Default.png",
-        "test": "../bitmaps_test/20161226-115400/Gridtest_Display_types_0_document_0_Default.png",
-        "selector": "document",
-        "fileName": "Gridtest_Display_types_0_document_0_Default.png",
-        "label": "Display types",
-        "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00",
-          "analysisTime": 106,
-          "getDiffImage": null
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/Gridtest_List_positions_0_document_0_Default.png",
-        "test": "../bitmaps_test/20161226-115400/Gridtest_List_positions_0_document_0_Default.png",
-        "selector": "document",
-        "fileName": "Gridtest_List_positions_0_document_0_Default.png",
-        "label": "List positions",
-        "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00",
-          "analysisTime": 125,
-          "getDiffImage": null
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/Gridtest_List_style_0_document_0_Default.png",
-        "test": "../bitmaps_test/20161226-115400/Gridtest_List_style_0_document_0_Default.png",
-        "selector": "document",
-        "fileName": "Gridtest_List_style_0_document_0_Default.png",
-        "label": "List style",
-        "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00",
-          "analysisTime": 105,
-          "getDiffImage": null
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/Gridtest_Positions_1_0_document_0_Default.png",
-        "test": "../bitmaps_test/20161226-115400/Gridtest_Positions_1_0_document_0_Default.png",
-        "selector": "document",
-        "fileName": "Gridtest_Positions_1_0_document_0_Default.png",
-        "label": "Positions 1",
         "misMatchThreshold": 0.1,
         "diff": {
           "isSameDimensions": true,
@@ -129,11 +45,53 @@ report({
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/Gridtest_Positions_2_0_document_0_Default.png",
-        "test": "../bitmaps_test/20161226-115400/Gridtest_Positions_2_0_document_0_Default.png",
+        "reference": "../bitmaps_reference/Gridtest_Display_types_0_document_0_Default.png",
+        "test": "../bitmaps_test/20161226-122216/Gridtest_Display_types_0_document_0_Default.png",
         "selector": "document",
-        "fileName": "Gridtest_Positions_2_0_document_0_Default.png",
-        "label": "Positions 2",
+        "fileName": "Gridtest_Display_types_0_document_0_Default.png",
+        "label": "Display types",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 99,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/Gridtest_List_positions_0_document_0_Default.png",
+        "test": "../bitmaps_test/20161226-122216/Gridtest_List_positions_0_document_0_Default.png",
+        "selector": "document",
+        "fileName": "Gridtest_List_positions_0_document_0_Default.png",
+        "label": "List positions",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 113,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/Gridtest_List_style_0_document_0_Default.png",
+        "test": "../bitmaps_test/20161226-122216/Gridtest_List_style_0_document_0_Default.png",
+        "selector": "document",
+        "fileName": "Gridtest_List_style_0_document_0_Default.png",
+        "label": "List style",
         "misMatchThreshold": 0.1,
         "diff": {
           "isSameDimensions": true,
@@ -150,8 +108,50 @@ report({
     },
     {
       "pair": {
+        "reference": "../bitmaps_reference/Gridtest_Positions_1_0_document_0_Default.png",
+        "test": "../bitmaps_test/20161226-122216/Gridtest_Positions_1_0_document_0_Default.png",
+        "selector": "document",
+        "fileName": "Gridtest_Positions_1_0_document_0_Default.png",
+        "label": "Positions 1",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 85,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/Gridtest_Positions_2_0_document_0_Default.png",
+        "test": "../bitmaps_test/20161226-122216/Gridtest_Positions_2_0_document_0_Default.png",
+        "selector": "document",
+        "fileName": "Gridtest_Positions_2_0_document_0_Default.png",
+        "label": "Positions 2",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 109,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
         "reference": "../bitmaps_reference/Gridtest_Resets_0_document_0_Default.png",
-        "test": "../bitmaps_test/20161226-115400/Gridtest_Resets_0_document_0_Default.png",
+        "test": "../bitmaps_test/20161226-122216/Gridtest_Resets_0_document_0_Default.png",
         "selector": "document",
         "fileName": "Gridtest_Resets_0_document_0_Default.png",
         "label": "Resets",
@@ -163,7 +163,7 @@ report({
             "height": 0
           },
           "misMatchPercentage": "0.00",
-          "analysisTime": 134,
+          "analysisTime": 107,
           "getDiffImage": null
         }
       },
@@ -172,7 +172,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/Gridtest_Text_alignment_0_document_0_Default.png",
-        "test": "../bitmaps_test/20161226-115400/Gridtest_Text_alignment_0_document_0_Default.png",
+        "test": "../bitmaps_test/20161226-122216/Gridtest_Text_alignment_0_document_0_Default.png",
         "selector": "document",
         "fileName": "Gridtest_Text_alignment_0_document_0_Default.png",
         "label": "Text alignment",
@@ -184,7 +184,7 @@ report({
             "height": 0
           },
           "misMatchPercentage": "0.00",
-          "analysisTime": 91,
+          "analysisTime": 99,
           "getDiffImage": null
         }
       },
@@ -193,7 +193,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/Gridtest_Text_decoration_0_document_0_Default.png",
-        "test": "../bitmaps_test/20161226-115400/Gridtest_Text_decoration_0_document_0_Default.png",
+        "test": "../bitmaps_test/20161226-122216/Gridtest_Text_decoration_0_document_0_Default.png",
         "selector": "document",
         "fileName": "Gridtest_Text_decoration_0_document_0_Default.png",
         "label": "Text decoration",
@@ -205,7 +205,7 @@ report({
             "height": 0
           },
           "misMatchPercentage": "0.00",
-          "analysisTime": 102,
+          "analysisTime": 132,
           "getDiffImage": null
         }
       },
@@ -214,7 +214,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/Gridtest_Text_style_0_document_0_Default.png",
-        "test": "../bitmaps_test/20161226-115400/Gridtest_Text_style_0_document_0_Default.png",
+        "test": "../bitmaps_test/20161226-122216/Gridtest_Text_style_0_document_0_Default.png",
         "selector": "document",
         "fileName": "Gridtest_Text_style_0_document_0_Default.png",
         "label": "Text style",
@@ -226,7 +226,7 @@ report({
             "height": 0
           },
           "misMatchPercentage": "0.00",
-          "analysisTime": 102,
+          "analysisTime": 127,
           "getDiffImage": null
         }
       },
@@ -235,7 +235,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/Gridtest_Text_transform_0_document_0_Default.png",
-        "test": "../bitmaps_test/20161226-115400/Gridtest_Text_transform_0_document_0_Default.png",
+        "test": "../bitmaps_test/20161226-122216/Gridtest_Text_transform_0_document_0_Default.png",
         "selector": "document",
         "fileName": "Gridtest_Text_transform_0_document_0_Default.png",
         "label": "Text transform",
@@ -247,7 +247,7 @@ report({
             "height": 0
           },
           "misMatchPercentage": "0.00",
-          "analysisTime": 120,
+          "analysisTime": 154,
           "getDiffImage": null
         }
       },
@@ -256,7 +256,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/Gridtest_Text_words_0_document_0_Default.png",
-        "test": "../bitmaps_test/20161226-115400/Gridtest_Text_words_0_document_0_Default.png",
+        "test": "../bitmaps_test/20161226-122216/Gridtest_Text_words_0_document_0_Default.png",
         "selector": "document",
         "fileName": "Gridtest_Text_words_0_document_0_Default.png",
         "label": "Text words",
@@ -268,7 +268,7 @@ report({
             "height": 0
           },
           "misMatchPercentage": "0.00",
-          "analysisTime": 111,
+          "analysisTime": 140,
           "getDiffImage": null
         }
       },
@@ -277,7 +277,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/Gridtest_Visibility_0_document_0_Default.png",
-        "test": "../bitmaps_test/20161226-115400/Gridtest_Visibility_0_document_0_Default.png",
+        "test": "../bitmaps_test/20161226-122216/Gridtest_Visibility_0_document_0_Default.png",
         "selector": "document",
         "fileName": "Gridtest_Visibility_0_document_0_Default.png",
         "label": "Visibility",
@@ -289,7 +289,7 @@ report({
             "height": 0
           },
           "misMatchPercentage": "0.00",
-          "analysisTime": 105,
+          "analysisTime": 114,
           "getDiffImage": null
         }
       },
